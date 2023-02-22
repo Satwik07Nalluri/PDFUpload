8e6ce1b849ec1fb1e0178fcbce4c47d2f0bd6963
Moinuddin <E095880@mcgladrey.rsm.net>
2023-02-23 00:14:17 +0530

    Implemented File Stream

--- PDFUpload/Controllers/WeatherForecastController.cs
+++ PDFUpload/Controllers/WeatherForecastController.cs
@@ -15,11 +15,20 @@ namespace PDFUpload.Controllers
             _logger = logger;
         }
 
-        [HttpGet("{pdfloc}/{jsonloc}")]
-        public async Task<ActionResult<string>> PrintPdf(string pdfloc,string jsonloc)
+        [HttpPost]
+        public async Task<ActionResult<string>> PrintPdf(IFormFile pdf,IFormFile json,string destination_filename)
         {
-            MergeDocument sourcepdf = new MergeDocument(@pdfloc);
-            var file = System.IO.File.ReadAllText(@jsonloc);
+            var currentRoute = System.IO.Directory.GetCurrentDirectory();
+            string route = Path.Combine(currentRoute, "files");
+            var pdf_new = Path.Combine(route,$"{Guid.NewGuid()}{Path.GetExtension(pdf.FileName)}");
+            var json_new = Path.Combine(route, $"{Guid.NewGuid()}{Path.GetExtension(json.FileName)}");
+            
+
+            SaveFile saveFile = new SaveFile();
+            saveFile.save(pdf,pdf_new);
+            saveFile.save(json,json_new);
+            MergeDocument sourcepdf = new MergeDocument(@pdf_new);
+            var file = System.IO.File.ReadAllText(@json_new);
             var data = JsonSerializer.Deserialize<Dictionary<string, string>>(file);
 
             foreach (var (key, value) in data)
@@ -27,8 +36,12 @@ namespace PDFUpload.Controllers
                 sourcepdf.Form.Fields[key].Value = value;
             }
             //sourcepdf.Form.Fields["CheckBox1"].Value = "1";
-            sourcepdf.Draw(@"C:\Sai_Charan_Work\PrintPDF\Destination.pdf");
+            string destpdf = Path.Combine(route, destination_filename + ".pdf");
+            sourcepdf.Draw(@destpdf);
+            System.IO.File.Delete(@pdf_new);
+            System.IO.File.Delete(@json_new);
             return "Success";
+
         }
     }
 }
---
+++ PDFUpload/SaveFile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PDFUpload
+{
+    public class SaveFile
+    {
+        public async Task<ActionResult<string>> save(IFormFile file, string fileLoc)
+        {
+            
+            using (FileStream fileStream = System.IO.File.Create(fileLoc))
+            {
+                await file.OpenReadStream().CopyToAsync(fileStream);
+            }
+            return "Success";
+        }
+    }
+}
