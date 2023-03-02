181b7f5d8a88e48f0426f6811b671939d9fc2da8
Saicharan Reddy <E095884@mcgladrey.rsm.net>
2023-03-02 23:45:55 +0530

    Storing the files in Temp Folder and extracting json data

--- PDFUpload/Controllers/PrintPDF.cs
+++ PDFUpload/Controllers/PrintPDF.cs
@@ -1,6 +1,8 @@
 using ceTe.DynamicPDF.Merger;
+using ceTe.DynamicPDF.PageElements;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
+using Newtonsoft.Json;
+
 
 namespace PDFUpload.Controllers
 {
@@ -14,45 +16,68 @@ namespace PDFUpload.Controllers
         {
             _logger = logger;
         }
-
         [HttpPost]
-        public async Task<ActionResult> PrintPdf(IFormFile pdf, IFormFile json, string destination_filename)
+        public async Task<ActionResult> PrintPdf(IFormFile pdf, IFormFile json)
         {
-            var currentRoute = System.IO.Directory.GetCurrentDirectory();
-            string route = Path.Combine(currentRoute, "files");
-            var pdf_new = Path.Combine(route, $"{Guid.NewGuid()}{Path.GetExtension(pdf.FileName)}");
-            var json_new = Path.Combine(route, $"{Guid.NewGuid()}{Path.GetExtension(json.FileName)}");
+            // Getting Path for the file
+            string? currentRoute = System.IO.Directory.GetCurrentDirectory();
 
+            // Folder that holds all temporary folders
+            string? tempFolderRoute = System.IO.Path.Combine(currentRoute, "TemporaryFodlers");
+            if(!Directory.Exists(tempFolderRoute))
+            {
+                Directory.CreateDirectory(tempFolderRoute);
+            }
 
+            // Creating Object of SaveFile Class that helps us to save files at a given location
             SaveFile saveFile = new SaveFile();
-            await saveFile.save(pdf, pdf_new);
-            await saveFile.save(json, json_new);
-            MergeDocument sourcepdf = new MergeDocument(@pdf_new);
-            for(var i = 0;i<=0;i++)
-            {
-                sourcepdf.Append(pdf_new);
-            }
-            var file = System.IO.File.ReadAllText(@json_new);
-            var data = JsonSerializer.Deserialize<Dictionary<string, string>>(file);
+            string files_loc = await saveFile.saveBoth(pdf, json, tempFolderRoute);
 
-            var bytes = await System.IO.File.ReadAllBytesAsync(pdf_new);
-            var returnValue = File(bytes, "application/pdf", destination_filename + ".pdf");
+            // Get JSON data
+            PdfProperties pdfProperties = new PdfProperties(files_loc);
 
-            if (data is not null)
+
+
+            // Printing Data Extracted to Single Page Pdfs in files_loc folder
+            for(int i=1; i <= 3;i++)
             {
-                foreach (var (key, value) in data)
+                foreach(var (k,v) in pdfProperties.GetPdfpropsofPage(i))
                 {
-                    sourcepdf.Form.Fields[key].Value = value;
+                    Console.WriteLine($"{k} is {v}");
                 }
-                string destpdf = Path.Combine(route, destination_filename + ".pdf");
-                sourcepdf.Draw(@destpdf);
-                bytes = await System.IO.File.ReadAllBytesAsync(destpdf);
-                returnValue = File(bytes, "application/pdf", Path.GetFileName(destpdf));
-                System.IO.File.Delete(@pdf_new);
-                System.IO.File.Delete(@json_new);
-                System.IO.File.Delete(destpdf);
             }
-            return returnValue;
+
+            // Merging Pdfs and Deleting Temp Folder and get File as return value
+
+
+
+
+
+
+            //return File
+            return Ok("Success");
+            //MergeDocument sourcepdf = new MergeDocument(@pdf_new);
+            //var file = System.IO.File.ReadAllText(@json_new);
+            //var data = JsonConvert.DeserializeObject<Dictionary<string,string>>(file);
+
+            //var bytes = await System.IO.File.ReadAllBytesAsync(pdf_new);
+            //var returnValue = File(bytes, "application/pdf", destination_filename + ".pdf");
+
+            //if (data is not null)
+            //{
+            //    foreach (var (key, value) in data)
+            //    {
+            //        sourcepdf.Form.Fields[key].Value = value;
+
+            //    }                string destpdf = Path.Combine(route, destination_filename + ".pdf");
+            //    sourcepdf.Draw(@destpdf);
+            //    bytes = await System.IO.File.ReadAllBytesAsync(destpdf);
+            //    returnValue = File(bytes, "application/pdf", Path.GetFileName(destpdf));
+            //    System.IO.File.Delete(@pdf_new);
+            //    System.IO.File.Delete(@json_new);
+            //    System.IO.File.Delete(destpdf);
+            //}
+            //return returnValue;
 
         }
     }
--- PDFUpload/SaveFile.cs
+++ PDFUpload/SaveFile.cs
@@ -4,14 +4,57 @@ namespace PDFUpload
 {
     public class SaveFile
     {
+        public async Task<string> saveBoth(IFormFile pdffile, IFormFile jsonfile, string currentRoute)
+        {
+            string nowString = DateTime.Now.ToString("MM_dd_HH_mm_ss");
+
+            string route = Path.Combine(currentRoute, "TempLoc_" + nowString);
+
+            Directory.CreateDirectory(route);
+
+            // Saving Pdf Files
+            for (var i = 1; i < 3; i++)
+            {
+                var pdf_loc = Path.Combine(route, $"1{Path.GetExtension(pdffile.FileName)}");
+                await save(pdffile, pdf_loc);
+            }
+
+
+            // Saving JSON File
+            var jsonloc = Path.Combine(route, $"data{Path.GetExtension(jsonfile.FileName)}");
+            await save(jsonfile, jsonloc);
+
+            return route;
+        }
+
+
+
         public async Task<ActionResult<string>> save(IFormFile file, string fileLoc)
         {
-            
+
             using (FileStream fileStream = System.IO.File.Create(fileLoc))
             {
                 await file.OpenReadStream().CopyToAsync(fileStream);
             }
             return "Success";
         }
+
+        //public async Task<ActionResult<string>> SavePdf(IFormFile file, string currentRoute)
+        //{
+        //    using (FileStream fileStream = System.IO.File.Create(fileLoc))
+        //    {
+        //        await file.OpenReadStream().CopyToAsync(fileStream);
+        //    }
+        //    return "Success";
+        //}
+
+        //public async Task<ActionResult<string>> SaveJson(IFormFile file, string currentRoute)
+        //{
+        //    using (FileStream fileStream = System.IO.File.Create(fileLoc))
+        //    {
+        //        await file.OpenReadStream().CopyToAsync(fileStream);
+        //    }
+        //    return "Success";
+        //}
     }
 }
